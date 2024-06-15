abee563adf58613471b58e2b7ed9158470e9d52d
Rohan Maan <rohanmandal2208@gmail.com>
2024-06-15 23:24:48 +0530

    initial

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("search").onclick = function () {
+    window.location.href = "search.html"; // Replace with the URL of the page you want to open
+};
+document.getElementById("home").onclick = function () {
+    window.location.href = "index.html"; // Replace with the URL of the page you want to open
+};
+document.getElementById("tv").onclick = function () {
+    window.location.href = "tv.html"; // Replace with the URL of the page you want to open
+};
+document.getElementById("movies").onclick = function () {
+    window.location.href = "movies.html"; // Replace with the URL of the page you want to open
+};
+document.getElementById("sports").onclick = function () {
+    window.location.href = "sports.html"; // Replace with the URL of the page you want to open
+};
